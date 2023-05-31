b84a3e18c9f886003bf590b96c185f2deeb3eb28
wins <win4709hi@gmail.com>
2023-05-31 23:10:33 +0800

    refactor: remove no need code

--- delivers/ubereats.py
+++ delivers/ubereats.py
@@ -16,8 +16,6 @@ def get_dishes(restaurant_page_url: str) -> list[Dish]:
         json_data = json.loads(content.text)
         categories = json_data["hasMenu"]["hasMenuSection"]
 
-        print(categories)
-
         dishes = []
         for category in categories:
             category_name = category["name"]
@@ -33,12 +31,3 @@ def get_dishes(restaurant_page_url: str) -> list[Dish]:
         print("get dishes error")
         print(e)
         return []
-
-
-# dishes = get_dishes(
-#     "https://www.ubereats.com/tw/store/%E5%BF%A0%E5%91%88%E6%BA%AB%E5%B7%9E%E5%A4%A7%E9%A4%9B%E9%A3%A9/MIE0wXk4TYOxf5a_4ZJD4g?utm_campaign=place-action-link&utm_medium=organic&utm_source=google"
-# )
-
-# for dish in dishes:
-#     print("--------")
-#     dish.print()
